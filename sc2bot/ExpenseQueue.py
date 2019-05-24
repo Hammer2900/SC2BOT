5e19488e096e148c67bc1f00e5c7774f0f87aebe
Niels Wouda <nielswouda@gmail.com>
2019-05-24 22:07:09 +0200

    Queue to handle expense actions

---
+++ sc2bot/ExpenseQueue.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import Callable
+
+from sc2 import BotAI
+
+
+class ExpenseQueue:
+    """
+    Keeps track of expenses to be made.
+    """
+
+    def __init__(self, bot: BotAI):
+        self._bot = bot
+        self._queue = deque()
+
+    def put(self, action: Callable):
+        self._queue.appendleft(action)
+
+    def get(self) -> Callable:
+        return self._queue.pop()
+
+    def resolve(self):
+        """
+        Attempts to resolve the current queue in order.
+        """
+        while len(self._queue):
+            action = self.get()
+
+            if not action():                    # action did not succeed, so
+                self._queue.append(action)      # add it as first-most action
+                break                           # for the next iteration.
