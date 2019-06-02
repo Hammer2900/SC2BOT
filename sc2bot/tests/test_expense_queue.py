f677e8ace96cff1e34a5f3dbca592013e9b15b67
Niels Wouda <nielswouda@gmail.com>
2019-06-02 12:04:49 +0200

    More tests for ExpenseQueue

--- sc2bot/tests/test_expense_queue.py
+++ sc2bot/tests/test_expense_queue.py
@@ -12,3 +12,52 @@ def test_empty():
 
     queue.get()
     assert_(queue.is_empty())                   # and now it should be gone
+
+
+def test_put_get_same_result():
+    def sentinel(): pass
+
+    queue = ExpenseQueue(BotAI())
+    queue.put(sentinel)
+
+    assert_(queue.get() is sentinel)            # equality is insufficient!
+
+
+def test_resolve_empty():
+    queue = ExpenseQueue(BotAI())
+    queue.resolve()                             # this should not have effect
+
+
+def test_resolve_requeue():
+    def false(): return False
+
+    queue = ExpenseQueue(BotAI())
+
+    queue.put(false)                            # since the return value is
+    queue.resolve()                             # False, the action should
+    assert_(queue.get() is false)               # remain enqueued.
+
+
+def test_resolve():
+    def true(): return True
+
+    queue = ExpenseQueue(BotAI())
+
+    queue.put(true)                             # since the return value is
+    queue.resolve()                             # True, the action should
+    assert_(queue.is_empty())                   # have been processed.
+
+
+def test_is_fifo_queue():
+    def first(): return None
+
+    def second(): return None
+
+    assert_(first is not second)                # sanity check
+
+    queue = ExpenseQueue(BotAI())
+
+    queue.put(first)                            # queue should be FIFO; since
+    queue.put(second)                           # we placed first before second
+    assert_(queue.get() is first)               # we should get them back in
+    assert_(queue.get() is second)              # the same order.
